2b0bd7ecf61729c46a8fbc3aa51e7e7c9828da92
zoemccormick <zmm7@cornell.edu>
2020-08-19 17:11:19 -0400

    subnet ips, vars

--- main.tf
+++ main.tf
@@ -9,7 +9,7 @@ resource "aws_vpc" "vpc" {
   enable_dns_support   = true
   enable_dns_hostnames = true
   tags = {
-    Name = "ECS VPC"
+    Name = "Grey Matter ECS VPC"
   }
 }
 
@@ -20,6 +20,7 @@ resource "aws_internet_gateway" "internet_gateway" {
 resource "aws_subnet" "public" {
   vpc_id            = aws_vpc.vpc.id
   count             = "2"
+  map_public_ip_on_launch = true
   availability_zone = element(data.aws_availability_zones.available.names, count.index)
   cidr_block        = cidrsubnet(aws_vpc.vpc.cidr_block, ceil(log(2, 2)), count.index)
 }
@@ -48,4 +49,7 @@ module "greymatter" {
   subnets                      = [aws_subnet.public.0.id, aws_subnet.public.1.id]
   access_key_arn               = var.access_key_arn
   secret_access_key_arn        = var.secret_access_key_arn
+  service_role_arn             = var.service_role_arn
+  execution_role_arn           = var.execution_role_arn
+  docker_secret_arn            = var.docker_secret_arn
 }
